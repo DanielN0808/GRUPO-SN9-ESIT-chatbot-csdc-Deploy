6435dd2c0b237fcbb899e5a5a3b897c47888ba0e
DanielN0808 <danieldna08@gmail.com>
2026-02-04 09:41:47 -0600

    Fix conexión MySQL Railway

--- bot/db.py
+++ bot/db.py
@@ -6,10 +6,11 @@ load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT"))
     )
 
 def register_request(user_id, nombre, correo, tipo_solicitud, detalle):
